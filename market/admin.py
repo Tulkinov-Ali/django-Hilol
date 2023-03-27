ff591268cb2e38c2e0054218060d8dcd1c9818c8
Tulkinov Akbarali <77823386+Tulkinov-Ali@users.noreply.github.com>
2023-03-27 22:11:16 +0500

    add_requirements

--- market/admin.py
+++ market/admin.py
@@ -3,14 +3,17 @@ from .models import *
 
 
 class OrdersAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'place', 'courier', 'status']
     list_display = ['id', 'place', 'courier', 'status', 'order_time']
 
 
 class FoodsAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name', 'category', 'price', 'type', 'img']
     list_display = ['id', 'name', 'category', 'price', 'type', 'img']
 
 
 class MerchantAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name', 'debt']
     list_display = ['id', 'name', 'debt']
 
 
